2288cefa0a19a606facf341fc9c76ae29b12849a
maijaelisa <127877273+maijaelisa@users.noreply.github.com>
2023-12-30 16:24:58 +0200

    first commit

---
+++ peli.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+// Luo yhteyspooli MySQL-tietokantaan
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Tervetuloa peliin! Käytä /questions reittiä nähdäksesi kysymykset.');
+});
+
+
+// Määritä REST-endpointit
+
+
+app.get('/questions', async (req, res) => {
+  try {
+    const [results] = await pool.query('SELECT * FROM kysymykset');
+    res.json(results);
+  } catch (err) {
+    console.error('Virhe tietokantakyselyssä: ' + err.message);
+    res.status(500).json({ error: 'Tietokantavirhe' });
+  }
+});
+
+app.post('/questions', async (req, res) => {
+  const { text, answer } = req.body;
+  const sql = 'INSERT INTO kysymykset (kysymysteksti, vastaus) VALUES (?, ?)';
+  const values = [text, answer];
+
+  try {
+    await pool.query(sql, values);
+    res.json({ message: 'Kysymys lisätty onnistuneesti' });
+  } catch (err) {
+    console.error('Virhe tietokantakyselyssä: ' + err.message);
+    res.status(500).json({ error: 'Tietokantavirhe' });
+  }
+});
+
+
+
+// Lisää muita endpointteja tarpeen mukaan
+
+// Käynnistä Express-sovellus
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ pelitietokanta.sql
@@ -0,0 +1,19 @@
+-- Active: 1701249799872@@127.0.0.1@3306@pelitietokanta
+-- Luo tietokanta, jos se ei ole jo olemassa
+CREATE DATABASE IF NOT EXISTS pelitietokanta;
+
+-- Valitse tietokantaa käytettäväksi
+USE pelitietokanta;
+
+-- Luo taulu kysymyksille
+CREATE TABLE IF NOT EXISTS kysymykset (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  kysymysteksti VARCHAR(255) NOT NULL,
+  vastaus VARCHAR(255) NOT NULL
+);
+
+-- Lisää esimerkkikysymyksiä
+INSERT INTO kysymykset (kysymysteksti, vastaus) VALUES
+  ('Mikä on Suomen pääkaupunki?', 'Helsinki'),
+  ('Mikä on 2 + 2?', '4'),
+  ('Mikä on suurin planeetta aurinkokunnassamme?', 'Jupiter');
